465748e75c939be1e0641ee01c3882299b4d4bc4
Cardell <kingccj2@gmail.com>
2020-12-16 00:24:06 -0600

    add jump restriction to the player to limit to one jump. Add spawn manager. Add movement to both the background and the obstacle that is spawned to create scrolling effect.

---
+++ Assets/Scripts/SpawnManager.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnManager : MonoBehaviour
+{
+    public GameObject obstaclePrefab;
+    private Vector3 spawnPOS = new Vector3(25, 0, 0);
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Instantiate(obstaclePrefab, spawnPOS, obstaclePrefab.transform.rotation);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
